ceb640df0fba7abeefa9cb2c44d7ab79bd941cd8
Aman Prajapati <amanprajapati22@gmail.com>
2026-01-06 15:49:22 +0530

    Livekit Server Ready

---
+++ agent.py
@@ -0,0 +1,46 @@
+from dotenv import load_dotenv
+
+from livekit import agents, rtc
+from livekit.agents import AgentServer, AgentSession, Agent, room_io
+from livekit.plugins import (
+    openai,
+    google,
+    noise_cancellation,
+)
+
+from prompts import AGENT_INSTRUCTION, SESSION_INSTRUCTION
+
+load_dotenv()
+
+class Assistant(Agent):
+    def __init__(self) -> None:
+        super().__init__(instructions=AGENT_INSTRUCTION, 
+                         llm=openai.realtime.RealtimeModel(
+                             voice="alloy",
+                             temperature=0.9
+                         )
+        )
+
+server = AgentServer()
+
+@server.rtc_session()
+async def my_agent(ctx: agents.JobContext):
+    session = AgentSession()
+
+    await session.start(
+        room=ctx.room,
+        agent=Assistant(),
+        room_options=room_io.RoomOptions(
+            audio_input=room_io.AudioInputOptions(
+                noise_cancellation=lambda params: noise_cancellation.BVCTelephony() if params.participant.kind == rtc.ParticipantKind.PARTICIPANT_KIND_SIP else noise_cancellation.BVC(),
+            ),
+        ),
+    )
+
+    await session.generate_reply(
+        instructions=SESSION_INSTRUCTION
+    )
+
+
+if __name__ == "__main__":
+    agents.cli.run_app(server)
